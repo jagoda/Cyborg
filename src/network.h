e13b0ef4671da05b3f0ca67f795a31c3d85da81c
Jeff Jagoda <jeffrey.jagoda@gmail.com>
2011-07-13 11:05:03 -0400

    implemented network_my_addresses

--- src/network.h
+++ src/network.h
@@ -6,4 +6,6 @@
 
 ip4_config ** network_my_addresses ();
 
+void network_free_addresses (ip4_config ** addresses);
+
 #endif /* NETWORK_H */
--- tests/network.c
+++ tests/network.c
@@ -4,12 +4,34 @@
 #include "../src/network-manager.h"
 
 
+static GDBusConnection * connection;
+
+static void setup ();
+static void teardown ();
+
+
+void setup ()
+{
+    g_debug("preparing for test run");
+    connection = network_manager_init();
+    fail_if(connection == NULL, "failed to connect to DBus");
+}
+
+void teardown ()
+{
+    gboolean closed;
+
+    g_debug("cleaning up test run");
+    closed = g_dbus_connection_close_sync(connection, NULL, NULL);
+    fail_unless(closed == TRUE, "Failed to close the DBus connection");
+}
+
 START_TEST(test_my_addresses)
 {
     ip4_config ** addresses, ** address_pointer;
     gint address_count;
 
-    addresses = network_my_addresses();
+    addresses = network_my_addresses(connection);
     fail_if(addresses == NULL, "network_my_addresses returned NULL.");
     for (
             address_count = 0, address_pointer = addresses;
@@ -17,6 +39,7 @@ START_TEST(test_my_addresses)
             address_pointer++, address_count++
         );
     fail_unless(address_count > 0, "Really? No local addresses?");
+    network_free_addresses(addresses);
 }
 END_TEST
 
@@ -26,6 +49,7 @@ TCase * network_core_testcase ()
     TCase * testcase;
 
     testcase = tcase_create("Core");
+    tcase_add_checked_fixture(testcase, setup, teardown);
     tcase_add_test(testcase, test_my_addresses);
 
     return testcase;
